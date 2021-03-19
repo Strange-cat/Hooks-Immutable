a2df02b639b2f2e1c479f92f3b83097274fcafca
bucai <2939137282@qq.com>
2021-03-19 16:30:45 +0800

    横向分类列表开发

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { memo, useState } from "react";
+import Horizen from "../../baseUI/horizen-item";
+import { categoryTypes, alphaTypes } from "../../api/config";
+import { NavContainer } from "./style";
 
-function Singers(props) {
-    return (
-        <div>Singers</div>
-    )
+function Singers() {
+  let [category, setCategory] = useState("");
+  let [alpha, setAlpha] = useState("");
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val);
+  };
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val);
+  };
+
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门):"}
+        oldVal={category}
+        handleClick={(val) => handleUpdateCatetory(val)}
+      ></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        oldVal={alpha}
+        handleClick={(val) => handleUpdateAlpha(val)}
+      ></Horizen>
+    </NavContainer>
+  );
 }
 
-export default React.memo(Singers);
+export default memo(Singers);
