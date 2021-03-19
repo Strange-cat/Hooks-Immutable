aa2106b365ccca653ed57c67314e21fe3a4249ac
bucai <2939137282@qq.com>
2021-03-19 18:07:53 +0800

    歌手列表

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,12 +1,25 @@
 import React, { memo, useState } from "react";
 import Horizen from "../../baseUI/horizen-item";
 import { categoryTypes, alphaTypes } from "../../api/config";
-import { NavContainer } from "./style";
+import { NavContainer, List, ListContainer, ListItem } from "./style";
+import Scroll from "../../baseUI/scroll";
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEnterLoading,
+  changePageCount,
+  refreshMoreSingerList,
+  changePullUpLoading,
+  changePullDownLoading,
+  refreshMoreHotSingerList,
+} from "./store/actionCreators";
+
+import { useSelector, useDispatch } from "react-redux";
 
 function Singers() {
   let [category, setCategory] = useState("");
   let [alpha, setAlpha] = useState("");
-
+  const dispatch = useDispatch();
   let handleUpdateAlpha = (val) => {
     setAlpha(val);
   };
@@ -15,21 +28,59 @@ function Singers() {
     setCategory(val);
   };
 
+  //mock 数据
+  const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
+    return {
+      picUrl:
+        "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "隔壁老樊",
+      accountId: 277313426,
+    };
+  });
+
+  // 渲染函数，返回歌手列表
+  const renderSingerList = () => {
+    return (
+      <List>
+        {singerList.map((item, index) => {
+          return (
+            <ListItem key={item.accountId + "" + index}>
+              <div className="img_wrapper">
+                <img
+                  src={`${item.picUrl}?param=300x300`}
+                  width="100%"
+                  height="100%"
+                  alt="music"
+                />
+              </div>
+              <span className="name">{item.name}</span>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+
   return (
-    <NavContainer>
-      <Horizen
-        list={categoryTypes}
-        title={"分类 (默认热门):"}
-        oldVal={category}
-        handleClick={(val) => handleUpdateCatetory(val)}
-      ></Horizen>
-      <Horizen
-        list={alphaTypes}
-        title={"首字母:"}
-        oldVal={alpha}
-        handleClick={(val) => handleUpdateAlpha(val)}
-      ></Horizen>
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizen
+          list={categoryTypes}
+          title={"分类 (默认热门):"}
+          oldVal={category}
+          handleClick={(val) => handleUpdateCatetory(val)}
+        ></Horizen>
+        <Horizen
+          list={alphaTypes}
+          title={"首字母:"}
+          oldVal={alpha}
+          handleClick={(val) => handleUpdateAlpha(val)}
+        ></Horizen>
+      </NavContainer>
+      <ListContainer>
+        <Scroll>{renderSingerList()}</Scroll>
+      </ListContainer>
+    </div>
   );
 }
 
