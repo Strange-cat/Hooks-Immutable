8076239f26c023f53eb76c3b88e2f4d59b702efc
bucai <2939137282@qq.com>
2021-03-19 15:16:36 +0800

    数据层

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,39 +7,36 @@
  * @FilePath: \cloud-music\src\routes\index.js
  */
 //routes/index.js
-import React from 'react';
+import React from "react";
 import { Redirect } from "react-router-dom";
-import Home from '../application/Home';
-import Recommend from '../application/Recommend';
-import Singers from '../application/Singers';
-import Rank from '../application/Rank';
+import Home from "../application/Home";
+import Recommend from "../application/Recommend";
+import Singers from "../application/Singers";
+import Rank from "../application/Rank";
 
-const arr = [
-    {
+// eslint-disable-next-line
+export default [
+  {
+    path: "/",
+    component: Home,
+    routes: [
+      {
         path: "/",
-        component: Home,
-        routes: [
-            {
-                path: "/",
-                exact: true,
-                render: () => (
-                    <Redirect to={"/recommend"} />
-                )
-            },
-            {
-                path: "/recommend",
-                component: Recommend
-            },
-            {
-                path: "/singers",
-                component: Singers
-            },
-            {
-                path: "/rank",
-                component: Rank
-            }
-        ]
-    }
-]
-
-export default arr
+        exact: true,
+        render: () => <Redirect to={"/recommend"} />,
+      },
+      {
+        path: "/recommend",
+        component: Recommend,
+      },
+      {
+        path: "/singers",
+        component: Singers,
+      },
+      {
+        path: "/rank",
+        component: Rank,
+      },
+    ],
+  },
+];
